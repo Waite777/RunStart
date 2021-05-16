2ad387142d823ac86996caf2dc32a25b82630dbd
Waite777 <rew2794@gmail.com>
2021-05-16 16:55:32 +0300

    js + content

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,26 @@ $(document).ready(function () {
          }
       ]
    });
+
+   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
+      $(this)
+         .addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+         .closest('div.container').find('div.catalog__contant').removeClass('catalog__contant-active').eq($(this).index()).addClass('catalog__contant-active');
+   });
+
+   $('.catalog-item__link').each(function (i) {
+      $(this).on('click', function (e) {
+         e.preventDefault();
+         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
+         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
+      })
+   })
+
+   $('.catalog-item__back').each(function (i) {
+      $(this).on('click', function (e) {
+         e.preventDefault();
+         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
+         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
+      })
+   })
 });
