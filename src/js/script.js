e6ff2f5cdc6e4ed2862ade7dc3055f8ab1bf691f
Waite777 <rew2794@gmail.com>
2021-05-18 22:32:09 +0300

    animation + scroll

--- src/js/script.js
+++ src/js/script.js
@@ -103,4 +103,21 @@ $(document).ready(function () {
       });
       return false;
    });
+
+   //Scroll
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1000) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();
+      }
+   });
+
+   $("a[href^='#'], #catalog").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
+
 });
