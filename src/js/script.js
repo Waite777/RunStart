bfa8b85dc79103cc762180fdb8e1a8a6f0f3a135
Waite777 <rew2794@gmail.com>
2021-05-18 17:26:49 +0300

    mailer + pageup

--- src/js/script.js
+++ src/js/script.js
@@ -88,4 +88,19 @@ $(document).ready(function () {
    validateForms('#order form');
 
    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+   $('form').submit(function () {
+      e.preventDefault();
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+      }).done(function () {
+         $(this).find("input").val("")
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
+         $('form').trigger('reset');
+      });
+      return false;
+   });
 });
